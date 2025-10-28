59885ea48f44f0953c389d854386bb17d293a1e5
santiago ortiz garcia <142601286+Santi-1604@users.noreply.github.com>
2025-10-27 23:59:45 -0600

    Se decidio comentar la parte del backtesting porque no todos van a iniciar con el modelo ya listo de mlflow y eso es vital para que esta parte corra por lo cual se indicara que hacer en el readme

--- main.py
+++ main.py
@@ -34,31 +34,31 @@ def main():
     m_cnn = modelo_cnn(df_estandar, 'signal', 50, 32, 0.1, 0.6, 0.2,
                        'drift_report_cnn', w, 6, 3)
 
-    print(" Cargando rutas a modelos (MLflow artifacts)...")
-    ruta_mlp = ('file:///C:/Users/52331/PycharmProjects/PythonProject4/'
-                'Proyecto-3/mlruns/178340679539533950/models/'
-                'm-0b8695dbd759460981fa0c73f11eeb48/artifacts')
+#    print(" Cargando rutas a modelos (MLflow artifacts)...")
+#    ruta_mlp = ('file:///C:/Users/52331/PycharmProjects/PythonProject4/'
+#                'Proyecto-3/mlruns/178340679539533950/models/'
+#                'm-0b8695dbd759460981fa0c73f11eeb48/artifacts')
 
-    ruta_cnn = ('file:///C:/Users/52331/PycharmProjects/PythonProject4/'
-                'Proyecto-3/mlruns/560905997404354661/models/'
-                'm-3183024883b64e599cdcdb94d5546fb7/artifacts')
+#    ruta_cnn = ('file:///C:/Users/52331/PycharmProjects/PythonProject4/'
+#                'Proyecto-3/mlruns/560905997404354661/models/'
+#                'm-3183024883b64e599cdcdb94d5546fb7/artifacts')
 
-    print(" Evaluando modelo MLP...")
-    r_df_real, met_real, r_df_mlp, met_mlp = eval_model(
-        df_estandar, df_real, ruta_mlp, 0.8, 'signal',
-        100000, 0.0125, 10, 0.05, 0.05, 0.0025, 0.2, 1
-    )
+#    print(" Evaluando modelo MLP...")
+#    r_df_real, met_real, r_df_mlp, met_mlp = eval_model(
+#        df_estandar, df_real, ruta_mlp, 0.8, 'signal',
+#        100000, 0.0125, 10, 0.05, 0.05, 0.0025, 0.2, 1
+#    )
 
-    print(" Evaluando modelo CNN...")
-    _, _, r_df_cnn, met_cnn = eval_model(
-        df_estandar, df_real, ruta_cnn, 0.8, 'signal',
-        100000, 0.0125, 10, 0.05, 0.05, 0.0025, 0.2, 2
-    )
+#    print(" Evaluando modelo CNN...")
+#    _, _, r_df_cnn, met_cnn = eval_model(
+#        df_estandar, df_real, ruta_cnn, 0.8, 'signal',
+#       100000, 0.0125, 10, 0.05, 0.05, 0.0025, 0.2, 2
+#    )
 
-    print("Proceso completado con éxito.")
-    print(" Resultados:")
-    print("   - Métricas MLP:", met_mlp)
-    print("   - Métricas CNN:", met_cnn)
+  #  print("Proceso completado con éxito.")
+# print(" Resultados:")
+  #  print("   - Métricas MLP:", met_mlp)
+  #  print("   - Métricas CNN:", met_cnn)
 
 
 if __name__ == "__main__":
